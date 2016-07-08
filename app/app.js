2d30242009b0734d911daef104899682b830171b
Gonzalo Pombo <gonpombo@gmail.com>
2016-07-08 02:25:15 -0300

    [ADD] Module mural with routes for differents rooms. - Room 1 /#/room1 - Room 2 /#/room2

---
+++ app/app.js
@@ -0,0 +1,18 @@
+angular
+	.module('mural', ['ui.router'])
+	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		// For unmatched routes
+		$urlRouterProvider.otherwise('/');
+		$stateProvider
+			.state('main', {
+				templateUrl: 'templates/main.html'
+			})
+			.state('main.room1', {
+				url: '/room1',
+				templateUrl: 'templates/room1.html'
+			})
+			.state('main.room2', {
+				url: '/room2',
+				templateUrl: 'templates/room2.html'
+			})
+	}]);
