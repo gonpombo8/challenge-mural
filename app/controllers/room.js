1e3ac56dcad3835bc83d9ee9590e3b803153aea2
Gonzalo Pombo <gonpombo@gmail.com>
2016-07-09 20:13:00 -0300

    [FIX] Room.js => var instead of lets.

--- app/controllers/room.js
+++ app/controllers/room.js
@@ -1,38 +1,36 @@
 'use strict';
-angular
-	.module('mural')
-	.controller('roomController', ['$state', '$scope', function($state, $scope) {
-		var url            = window.location.protocol +  "//" + window.location.host;
-		var socket         = io(url);
-		var updatingScroll = false;
-		var room           = $state.current.data.room;
-		var timeout;
+app.controller('roomController', ['$state', '$scope', function($state, $scope) {
+	var url            = window.location.protocol +  "//" + window.location.host;
+	var socket         = io(url);
+	var updatingScroll = false;
+	var room           = $state.current.data.room;
+	var timeout;
 
-		socket.emit('room', room);
+	socket.emit('room', room);
 
-		window.onscroll = function() {
-			let scrollPercetage = window.pageYOffset / maxHeightScroll();
-			if(!updatingScroll) socket.emit('scrollChange', {percentage: scrollPercetage, room: room});
-		}
+	window.onscroll = function() {
+		var scrollPercetage = window.pageYOffset / maxHeightScroll();
+		if(!updatingScroll) socket.emit('scrollChange', {percentage: scrollPercetage, room: room});
+	}
 
-		socket.on('updateSroll', function(percentage) {
-			let height = percentage * maxHeightScroll();
-			updatingScroll = true;
-			clearTimeout(timeout);
-			timeout = setTimeout(function() {
-				updatingScroll = false;
-			}, 500);
-			window.scrollTo(0, height);
-		});
+	socket.on('updateSroll', function(percentage) {
+		var height = percentage * maxHeightScroll();
+		updatingScroll = true;
+		clearTimeout(timeout);
+		timeout = setTimeout(function() {
+			updatingScroll = false;
+		}, 500);
+		window.scrollTo(0, height);
+	});
 
-		function maxHeightScroll() {
-			let documentHeight = document.documentElement.scrollHeight;
-			let windowHeight = window.innerHeight;
-			return documentHeight - windowHeight;
-		}
+	function maxHeightScroll() {
+		var documentHeight = document.documentElement.scrollHeight;
+		var windowHeight = window.innerHeight;
+		return documentHeight - windowHeight;
+	}
 
-		$scope.$on('$destroy', function (event) {
-			socket.removeAllListeners();
-		});
+	$scope.$on('$destroy', function (event) {
+		socket.removeAllListeners();
+	});
 
-	}]);
+}]);
