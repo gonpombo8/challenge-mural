e5e4ef5528ffde0143ce51ae81ce52e657ab8e6f
Gonzalo Pombo <gonpombo@gmail.com>
2016-07-11 01:09:33 -0300

    [FIX] Inject bootstrap css with wiredep.

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,16 @@ gulp.task('run', () => {
 gulp.task('bower', () => {
 	gulp.src('app/index.html')
 	.pipe(wiredep({
+		"overrides": {
+			"bootstrap": {
+				"main": [
+				"dist/js/bootstrap.js",
+				"dist/css/bootstrap.css"
+				]
+			}
+		},
 		directory: 'app/bower_components'
-		}))
+	}))
 	.pipe(gulp.dest('app'));
 });
 
