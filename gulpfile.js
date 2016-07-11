450f2a2afcf75094f24b7ca26ec37f178c337939
Gonzalo Pombo <gonpombo@gmail.com>
2016-07-11 00:53:51 -0300

    [ADD] Bower wiredep for bower_components. Downgrade bootstrap version to 3.3.4.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp');
-var nodemon = require('gulp-nodemon')
+var nodemon = require('gulp-nodemon');
+var wiredep = require('wiredep').stream;
 
-gulp.task('serve', () => {
-  nodemon({
-    script: 'app.js'
-  })
+gulp.task('run', () => {
+	nodemon({
+		script: 'app.js'
+	})
 });
+
+gulp.task('bower', () => {
+	gulp.src('app/index.html')
+	.pipe(wiredep({
+		directory: 'app/bower_components'
+		}))
+	.pipe(gulp.dest('app'));
+});
+
+gulp.task('serve', ['bower', 'run']);
